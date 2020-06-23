a5632fdb7f6dfb59797977fc233f0a331ca1ca1a
Dmitriy Popko <popkodmitrii@yandex.ru>
2020-06-23 17:14:01 +0300

    Updates

---
+++ precache-manifest.d7747f72e36ff1c3b85038bcf815bc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0acfd23aa8225d9659ebf4d9a64fb0",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "3741aa9da12436c9d9d7",
+    "url": "/form/static/css/main.06cce6a1.chunk.css"
+  },
+  {
+    "revision": "9596a93415b83d2afeaf",
+    "url": "/form/static/js/2.9848803a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form/static/js/2.9848803a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3741aa9da12436c9d9d7",
+    "url": "/form/static/js/main.a7deb9c5.chunk.js"
+  },
+  {
+    "revision": "b0c6960af9845d6983bb",
+    "url": "/form/static/js/runtime-main.4549994f.js"
+  }
+]);
