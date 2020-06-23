cc8c8f74823aff4ccb40a31bde90be06d3602e15
Dmitriy Popko <popkodmitrii@yandex.ru>
2020-06-23 17:08:50 +0300

    first commit

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+function AddTodo( {onCreate}){
+
+    const [value, setValue] = useState('');
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        if(value.trim()){
+            onCreate(value)
+            setValue('')
+        }
+    }
+
+    return(
+        <form style ={{margin: '2rem'}} onSubmit = {submitHandler}  >
+            <input value = {value}  onChange = {e => setValue(e.target.value)} />
+            <button type = "submit">Add Todo</button>
+        </form>
+    )
+}
+
+
+AddTodo.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo;
